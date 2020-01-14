5deefd5f8693c3946daae703f8afbd6683c14961
Spencer Wood <spencer.wood@me.com>
2020-01-13 21:08:32 -0600

    added variables for begin/end

--- script.js
+++ script.js
@@ -1,20 +1,26 @@
 //variables user input
-// var term =
-// var recordCount =
-
-// var startYear =
-
-// var endYear =
+var term = $(".searchTerm");
+var recordCount = $(".retrieve");
+var startYear = $(".startYear");
+var endYear = $(".endYear");
 
 
 function nytSearch (term){
     
     var apiKey = "XIVhAvjVrJYaaW5HjYX599QmZTV3SUcG";
-    var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=election&api-key=https://api.nytimes.com/svc/search/v2/articlesearch.json?q=election&api-key=";
+    var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + term + "&api-key=" + apiKey;
     $.ajax({
-        url: queryURL;
+        url: queryURL,
         method: "GET"
     }).then(function(response){
         console.log(response);
     });
 };
+
+//abstarct
+
+//web_url
+
+//begin_date Begin date (e.g. 20120101) yyyymmdd
+
+//end_date End date (e.g. 20121231) yyyymmdd
